97cc4059b49e2fad9f8c5c90a0125a4f922584ca
Hachem <mohamad27hachem@outlook.com>
2022-01-25 22:39:11 +0100

    snakeUpdates

--- Snake_game/snake.py
+++ Snake_game/snake.py
@@ -3,7 +3,7 @@ from turtle import Turtle
 # creating our constants so we have more control over our game and not have to go and search in the code for changes
 STARTING_POSITIONS = [(0, 0),(-20, 0),(-40, 0)]
 MOVE_DISTANCE = 20
-up, down, left, right = 90, 270, 180, 0
+UP, DOWN, LEFT, RIGHT = 90, 270, 180, 0
 
 class Snake:
     # creating our class
@@ -47,17 +47,17 @@ class Snake:
 
     # creating a series of functions of movements for our snakes up ,down ,left ,right
     def move_up(self):
-        if(self.head.heading() != down):
+        if(self.head.heading() != DOWN):
             self.head.setheading(90)
 
     def move_right(self):
-        if (self.head.heading() != left):
+        if (self.head.heading() != LEFT):
             self.head.setheading(0)
 
     def move_left(self):
-        if (self.head.heading() != right):
+        if (self.head.heading() != RIGHT):
             self.head.setheading(180)
 
     def move_down(self):
-        if (self.head.heading() != up):
+        if (self.head.heading() != UP):
             self.head.setheading(270)
